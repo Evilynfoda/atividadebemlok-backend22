c5e3685a4a9457e73809da8e58a39d76a75c56c9
evilynfoda <evilyn.sutil@escola.pr.gov.br>
2026-05-08 10:48:45 -0300

    finalizado

--- server.js
+++ server.js
@@ -8,11 +8,31 @@ app.use(express.json());
 
 // Rota POST que implementa o método salvarContato() do Diagrama UML
 app.post('/finalizar-venda', (req, res) => {
-   
+
     // Agora usamos req.body porque os dados vêm escondidos do formulário HTML!
-        let nomeProduto = req.body.nomeProduto
-        let precoUnitario = req.body.precoUnitario
-        let quanidade = req.body.quantiade
+    let nomeProduto = req.body.nomeProduto;
+    let precoUnitario = req.body.precoUnitario;
+    let quantidade = req.body.quantiade;
+    let cupom = req.body.cupom;
+
+    let valorTotal = quantidade * precoUnitario;
+
+    if (quantiade < 1 || preco <= 0) {
+        res.status(400).json({
+            erro: "valor invalido",
+            mensagem: "Insira um valor valido"
+        })
+    }
+
+    if (cupom === "PROMO20") {
+        valorTotal = valorTotal * 0.8;
+        console.log("cupom aplicado! desconto de 20% consedido");
+    }
+
+    res.status(201).json({
+        mensagem: "venda processada com sucesso! o seu pedido foi criado ",
+        sucesso: "venda realizada com sucesso"
+    })
 
 });
 
